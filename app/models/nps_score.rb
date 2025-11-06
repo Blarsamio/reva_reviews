6dd407aad3dceb4d54c921f3fa813256af28053c
Patricio Perez Herrero <patriciopherrero@gmail.com>
2025-11-06 15:54:45 +0100

    Add validations and scopes to NpsScore model
    - Ensure presence and uniqueness of date scoped by company
    - Introduce scopes for global, company-specific, and date-based queries
    - Implement before_save callback to calculate and set NPS score based on promoter, passive, and detractor counts
    - Tested :)

--- app/models/nps_score.rb
+++ app/models/nps_score.rb
@@ -1,2 +1,25 @@
 class NpsScore < ApplicationRecord
+  validates :date, presence: true, uniqueness: { scope: :company }
+
+  scope :global, -> { where(company: nil) }
+  scope :by_company, -> { where.not(company: nil)}
+  scope :for_company, ->(name) { where(company: name) }
+  scope :for_date, ->(date) { where(date: date) }
+
+  before_save :set_score
+
+  def calculate_nps
+    total = promoter_count + passive_count + detractor_count
+    return 0 if total.zero?
+
+    # if I understand correctly, passives don't count on NPS score system
+    # so I can't keep them into account for numerator
+    ((promoter_count - detractor_count) / total.to_f * 100)
+  end
+
+  private
+
+  def set_score
+    self.score = calculate_nps
+  end
 end
