c364954438c69106aad4bd6da087d6506f550689
Patricio Perez Herrero <patriciopherrero@gmail.com>
2025-11-10 12:51:46 +0100

    Enhanced Pagy styling
    - Add custom styling for pagination using Pagy to improve user experience.
    - Update ReviewsController to allow pagination parameters in filter_params, resolving unpermitted params warning.
    - Revise index view to increase header size and center alignment, and update button styles for better visibility and interaction.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,14 +2,15 @@ class ReviewsController < ApplicationController
   def index
     @pagy, @reviews = pagy(Review.filtered(filter_params))
     @channels = Review.all.pluck(:channel).uniq
-    nps_loader = Nps::ScoreLoader.new(Date.yesterday)
+    nps_loader = Nps::ScoreLoader.new(Date.new(2025,02,02)) # passing a fixed dat to get NPS score, otherwise it should be Date.yesterday
     @global_nps = nps_loader.global_score
     @company_nps = nps_loader.company_scores
   end
 
   private
 
+  # had to add :page because from pagy i was getting unpermitted params :page warning
   def filter_params
-    params.permit(:company_name, :query, :start_date, :end_date, ratings: [], channels: [])
+    params.permit(:company_name, :query, :start_date, :end_date, :page, ratings: [], channels: [])
   end
 end
